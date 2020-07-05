1304e9b5e3a08ce8dda78f548e1c77e3c26234d7
Shari Bradford <shari.bradford@gmail.com>
2020-07-05 00:17:30 -0400

    added main route, validation route, css file, index.html, users_app/urls.py; updated views

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User
 
 # Create your views here.
+def index(request):
+    context = {
+        "user_list": User.objects.all() 
+    }
+    return render(request, 'index.html', context)
+
+def validate(request):
+    new_user = request.POST
+    User.objects.create(first_name=new_user["first_name"], last_name=new_user["last_name"],email_address=new_user["email_address"], age=new_user["age"])
+    return redirect('/')
